9087c1f87bd9a33cf55ee19878610d46be5baa3d
Luk Gastmans <lgastmans@gmail.com>
2024-06-13 13:45:16 +0530

    login page: change icon #4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Dry Herbarium - Auroville Botanical Garden</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
