3174a2062d1fbfc54ad393be2f61f249ebc3790a
Luk Gastmans <lgastmans@gmail.com>
2024-06-22 17:45:21 +0530

    Images upload for plants #3

--- resources/views/livewire/create-plant.blade.php
+++ resources/views/livewire/create-plant.blade.php
@@ -296,10 +296,10 @@
                     --}}
                     <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="images" role="tabpanel" aria-labelledby="images-tab">
 
-                        <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+                        <div class="grid grid-cols-2 md:grid-cols-2 gap-2">
                             @foreach($form->images as $image)
                                 <div>
-                                    <img class="h-auto max-w-full rounded-lg" src="{{ asset('Images/'.$image->filename) }}" alt="">
+                                    <img class="h-auto max-w-full rounded-lg" src="{{ asset('Images/'.$image->filename) }}" title="{{ $form->genus }}" alt="{{ $form->genus }}">
                                 </div>
                             @endforeach
                         </div>
