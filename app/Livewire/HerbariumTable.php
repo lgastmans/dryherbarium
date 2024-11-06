be250bb44c323f42e08d8cee61f548c111279929
Luk Gastmans <lgastmans@gmail.com>
2024-11-06 17:23:46 +0530

    Herbarium images #35

--- app/Livewire/HerbariumTable.php
+++ app/Livewire/HerbariumTable.php
@@ -371,7 +371,7 @@ final class HerbariumTable extends PowerGridComponent
         if (count($row->images) > 0)
             $svg = '<svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m3 16 5-7 6 6.5m6.5 2.5L16 13l-4.286 6M14 10h.01M4 19h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z"/>
-                        <circle cx="18" cy="6" r="7" fill="grey"/>
+                        <circle cx="18" cy="6" r="7" fill="green"/>
                         <text x="18" y="10" font-family="Arial" font-size="11" text-anchor="middle" fill="white">'.count($row->images).'</text>
                     </svg>';
         else
